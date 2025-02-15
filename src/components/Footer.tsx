faba2cc5d425698392358e7d69eaa46ca2e8ef5e
Dhruv <haldardhruv@gmail.com>
2025-02-15 21:28:26 +0530

    update Footer.tsx and ProjectCard.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,9 +24,9 @@ export const Footer = () => {
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
+          <Text onBackground="neutral-medium">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
-          <Text onBackground="neutral-weak">
+          <Text onBackground="neutral-medium">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
             / Made with{" "}
             <SmartLink
