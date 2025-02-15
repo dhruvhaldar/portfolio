c88c8c81bdb7dfc2bbad904b5cf54d100190a10f
Dhruv <haldardhruv@gmail.com>
2025-02-15 16:54:43 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,6 @@ options: {},
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 pageExtensions: ["ts", "tsx", "md", "mdx"],
-output: "export", // Manually specify output so GitHub's action won't try to inject it
 };
 
 export default withMDX(nextConfig);
