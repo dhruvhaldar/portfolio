4180b0a116d1f870609a5c9474f8b7b9ebde33ba
Dhruv <haldardhruv@gmail.com>
2025-02-20 03:17:55 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,6 @@ options: {},
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 pageExtensions: ["ts", "tsx", "md", "mdx"],
-async redirects() {
-    return [
-      {
-        source: "https://www.dhruvhaldar.vercel.app/:path*",
-        destination: "https://dhruvhaldar.vercel.app/:path*", // Redirect WWW to non-WWW
-        statusCode: 301,
-      },
-    ];
-  },
 };
 
 export default withMDX(nextConfig);
