570502f580034f4b88b3d3b773874ce128965060
Dhruv <haldardhruv@gmail.com>
2025-02-20 02:02:11 +0530

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,8 +64,7 @@ export default function Home() {
       />
       <Column fillWidth paddingY="l" gap="m">
         <Column maxWidth="s">
-        {/* Added an h1 tag */}
-          <h1>{home.title}</h1>
+        <h1 style={{ position: "absolute", left: "-9999px" }}>{home.title}</h1>
           <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
             <Heading wrap="balance" variant="display-strong-s">
               {home.headline}
