62ad086328a171f1fdea5361ae1df470c894fd2c
Dhruv <haldardhruv@gmail.com>
2025-02-20 20:40:37 +0530

    update page.tsx, page.tsx and src/app/resources/index.ts

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -16,6 +16,7 @@ export async function generateMetadata() {
       description,
       type: "website",
       url: `https://${baseURL}/gallery`,
+      alternates: {canonical: `${baseURL}/gallery/`},
       images: [
         {
           url: ogImage,
