52b31e66a3d8b7d4a428772574062d2aa7735e40
Dhruv <haldardhruv@gmail.com>
2025-02-22 23:20:20 +0530

    update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,7 +15,7 @@ export default async function sitemap() {
   const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route]);
 
   const routes = activeRoutes.map((route) => ({
-    url: `${baseURL}${route !== "/" ? route : ""}`,
+    url: `https://${baseURL}${route !== "/" ? route : ""}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
