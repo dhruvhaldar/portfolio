8e6dc167596fa9b1011295f250ad3cd49639a2bf
Dhruv <haldardhruv@gmail.com>
2025-02-23 12:43:52 +0530

    update Avatar.tsx

--- src/once-ui/components/Avatar.tsx
+++ src/once-ui/components/Avatar.tsx
@@ -7,91 +7,36 @@ const Avatar = forwardRef<HTMLDivElement, AvatarProps>(({ size = "m", value, src
 
     if (loading) {
       return (
-        <Skeleton
-          {...rest}
-          border="neutral-medium"
-          shape="circle"
-          width={size}
-          height={size}
-          className={`${styles.avatar} ${className}`}
-          aria-busy="true"
-          aria-label="Loading avatar"
-        />
+        <Skeleton {...rest} border="neutral-medium" shape="circle" width={size} height={size} className={`${styles.avatar} ${className}`} aria-busy="true" aria-label="Loading avatar"/>
       );
     }
 
     const renderContent = () => {
       if (isEmpty) {
-        return (
-          <Icon
-            onBackground="neutral-medium"
-            name="person"
-            size={size as "xs" | "s" | "m" | "l" | "xl"}
-            className={styles.icon}
-            aria-label="Empty avatar"
-          />
-        );
+        return (<Icon onBackground="neutral-medium" name="person" size={size as "xs" | "s" | "m" | "l" | "xl"} className={styles.icon} aria-label="Empty avatar"/>);
       }
 
       if (src) {
-        return (
-          <SmartImage
-            radius="full"
-            src={src}
-            fill
-            alt="Avatar"
-            sizes={`${sizeMapping[size]}px`}
-            className={styles.image}
-          />
-        );
+        return (<SmartImage radius="full" src={src} fill alt="Avatar" sizes={`${sizeMapping[size]}px`} className={styles.image}/>);
       }
 
       if (value) {
         return (
-          <Text
-            as="span"
-            onBackground="neutral-weak"
-            variant={`body-default-${size}`}
-            className={styles.value}
-            aria-label={`Avatar with initials ${value}`}
-          >
+          <Text as="span" onBackground="neutral-weak" variant={`body-default-${size}`} className={styles.value} aria-label={`Avatar with initials ${value}`}>
             {value}
           </Text>
         );
       }
-
       return null;
     };
 
     return (
-      <Flex
-        ref={ref}
-        role="img"
-        position="relative"
-        horizontal="center"
-        vertical="center"
-        radius="full"
-        border="neutral-strong"
-        background="surface"
-        style={style}
-        className={`${styles.avatar} ${styles[size]} ${className || ""}`}
-        {...rest}
-      >
+      <Flex ref={ref} role="img" position="relative" horizontal="center" vertical="center" radius="full" border="neutral-strong" background="surface" style={style} className={`${styles.avatar} ${styles[size]} ${className || ""}`} {...rest}>
         {renderContent()}
-        {statusIndicator && (
-          <StatusIndicator
-            size={statusIndicatorSizeMapping[size]}
-            color={statusIndicator.color}
-            className={`${styles.className || ""} ${styles.indicator} ${size === "xl" ? styles.position : ""}`}
-            aria-label={`Status: ${statusIndicator.color}`}
-          />
-        )}
+        {statusIndicator && (<StatusIndicator size={statusIndicatorSizeMapping[size]} color={statusIndicator.color} className={`${styles.className || ""} ${styles.indicator} ${size === "xl" ? styles.position : ""}`} aria-label={`Status: ${statusIndicator.color}`}/>)}
       </Flex>
     );
   },
 );
 
-Avatar.displayName = "Avatar";
-
-export { Avatar };
-export type { AvatarProps };
+Avatar.displayName = "Avatar";export { Avatar };export type { AvatarProps };
