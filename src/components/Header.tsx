39a422512c9dbc2d4dd2d401a08f20df0bb40415
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-01 14:19:23 +0530

    update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,24 +16,9 @@ export const Header = () => {
 
   return (
     <>
-      <Flex
-        fitHeight
-        className={styles.position}
-        as="header"
-        zIndex={9}
-        fillWidth
-        padding="8"
-        horizontal="center"
-      >
+      <Flex fitHeight className={styles.position} as="header" zIndex={9} fillWidth padding="8" horizontal="center">
         <Flex fillWidth horizontal="center">
-          <Flex
-            background="surface"
-            border="neutral-medium"
-            radius="m-4"
-            shadow="l"
-            padding="4"
-            horizontal="center"
-          >
+          <Flex background="surface" border="neutral-medium" radius="m-4" shadow="l" padding="4" horizontal="center">
             <Flex gap="4" vertical="center" textVariant="body-default-s">
               {routes["/"] && (<ToggleButton prefixIcon="home" href="/" label={home.label} selected={pathname === "/"} />)}
               <Line vert maxHeight="24" />
@@ -41,22 +26,8 @@ export const Header = () => {
                 <><ToggleButton className="s-flex-hide" prefixIcon="person" href="/about" label={about.label} selected={pathname === "/about"}/><ToggleButton className="s-flex-show" prefixIcon="person" href="/about" selected={pathname === "/about"}/></>
               )}
               {routes["/work"] && (
-                <>
-                  <ToggleButton className="s-flex-hide" prefixIcon="grid" href="/work" label={work.label} selected={pathname.startsWith("/work")}
-                  />
-                  <ToggleButton
-                    className="s-flex-show"
-                    prefixIcon="grid"
-                    href="/work"
-                    selected={pathname.startsWith("/work")}
-                  />
-                </>
+                <><ToggleButton className="s-flex-hide" prefixIcon="grid" href="/work" label={work.label} selected={pathname.startsWith("/work")}/>
+                  <ToggleButton className="s-flex-show" prefixIcon="grid" href="/work" selected={pathname.startsWith("/work")}/></>
               )}
               {/* Removed Blog and Gallery sections */}
-            </Flex>
-          </Flex>
-        </Flex>
-      </Flex>
-    </>
-  );
-};
+            </Flex></Flex></Flex></Flex></>);};
