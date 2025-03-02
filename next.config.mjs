beb989d441b2c8acd60a92f094c455bd44dbf520
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-02 16:04:04 +0530

    once ui update 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-import mdx from "@next/mdx";const withMDX = mdx({extension: /.mdx?$/,options: {},});
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {pageExtensions: ["ts", "tsx", "md", "mdx"],};
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+};
+
 export default withMDX(nextConfig);
