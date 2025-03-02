b4e4e53bb5deda8c8d6ec73f858569619514a574
Dhruv <haldardhruv@gmail.com>
2025-03-02 20:22:29 +0530

    update page.tsx, Header.module.scss and Header.tsx

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,12 +1,12 @@
-import { getPosts } from "@/app/utils/utils";
-import { Column } from "@/once-ui/components";
-import { Projects } from "@/components/work/Projects";
+import { Column, Flex, Heading } from "@/once-ui/components";
+import { Mailchimp } from "@/components";
+import { Posts } from "@/components/publications/Posts";
 import { baseURL } from "@/app/resources";
-import { person, work } from "@/app/resources/content";
+import { publications, person, newsletter } from "@/app/resources/content";
 
 export async function generateMetadata() {
-  const title = work.title;
-  const description = work.description;
+  const title = publications.title;
+  const description = publications.description;
   const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
 
   return {
@@ -16,8 +16,7 @@ export async function generateMetadata() {
       title,
       description,
       type: "website",
-      url: `https://${baseURL}/work/`,
-      alternates: {canonical: `${baseURL}/work/`},
+      url: `https://${baseURL}/publications`,
       images: [
         {
           url: ogImage,
@@ -34,37 +33,39 @@ export async function generateMetadata() {
   };
 }
 
-export default function Work() {
-  let allProjects = getPosts(["src", "app", "work", "projects"]);
-
+export default function Blog() {
   return (
-    <Column maxWidth="m">
+    <Column maxWidth="s">
       <script
         type="application/ld+json"
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "CollectionPage",
-            headline: work.title,
-            description: work.description,
-            url: `https://${baseURL}/projects`,
-            image: `${baseURL}/og?title=Design%20Projects`,
+            "@type": "Blog",
+            headline: publications.title,
+            description: publications.description,
+            url: `https://${baseURL}/publications`,
+            image: `${baseURL}/og?title=${encodeURIComponent(publications.title)}`,
             author: {
               "@type": "Person",
               name: person.name,
+              image: {
+                "@type": "ImageObject",
+                url: `${baseURL}${person.avatar}`,
+              },
             },
-            hasPart: allProjects.map((project) => ({
-              "@type": "CreativeWork",
-              headline: project.metadata.title,
-              description: project.metadata.summary,
-              url: `https://${baseURL}/projects/${project.slug}`,
-              image: `${baseURL}/${project.metadata.image}`,
-            })),
           }),
         }}
       />
-      <Projects />
+      <Heading marginBottom="l" variant="display-strong-s">
+        {publications.title}
+      </Heading>
+      <Column fillWidth flex={1}>
+        <Posts range={[1, 3]} thumbnail />
+        <Posts range={[4]} columns="2" />
+      </Column>
+      {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
 }
