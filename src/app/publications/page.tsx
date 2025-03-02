df86cbb247fe05fc86d6ea5538813c51bcf477f3
Dhruv Haldar <dhruv@sankhyasutralabs.com>
2025-03-02 18:41:12 +0530

    chore: delete implementation-of-wing-ice-protection-in-mea.mdx and page.js

---
+++ src/app/publications/page.tsx
@@ -0,0 +1,72 @@
+import { Column, Flex, Heading } from "@/once-ui/components";
+import { Mailchimp } from "@/components";
+import { Posts } from "@/components/blog/Posts";
+import { baseURL } from "@/app/resources";
+import { publications, person, newsletter } from "@/app/resources/content";
+
+export async function generateMetadata() {
+  const title = publications.title;
+  const description = publications.description;
+  const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      url: `https://${baseURL}/publications`,
+      alternates: {canonical: `${baseURL}/publications/`},
+      images: [
+        {
+          url: ogImage,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+    },
+  };
+}
+
+export default function Publications() {
+  return (
+    <Column maxWidth="s">
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Blog",
+            headline: publications.title,
+            description: publications.description,
+            url: `https://${baseURL}/publications`,
+            image: `${baseURL}/og?title=${encodeURIComponent(publications.title)}`,
+            author: {
+              "@type": "Person",
+              name: person.name,
+              image: {
+                "@type": "ImageObject",
+                url: `${baseURL}${person.avatar}`,
+              },
+            },
+          }),
+        }}
+      />
+      <Heading marginBottom="l" variant="display-strong-s">
+        {publications.title}
+      </Heading>
+      <Column fillWidth flex={1}>
+        <Posts range={[1, 3]} thumbnail />
+        <Posts range={[4]} columns="2" />
+      </Column>
+      {newsletter.display && <Mailchimp newsletter={newsletter} />}
+    </Column>
+  );
+}
