c01b59fa31c866b6f124a7fd0fd054791b78f95d
Dhruv <haldardhruv@gmail.com>
2025-03-03 12:07:20 +0530

    update page.tsx

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -76,7 +76,7 @@ export default function Publication() {
         </Flex>
         <Flex marginBottom="m">
         <SmartLink id="cy-effective-orcid-url" className="body-default-s" href="https://orcid.org/0000-0002-2734-313X" target="orcid.widget" rel="me noopener noreferrer" style={{ verticalAlign: "top" }} suffixIcon="arrowUpRightFromSquare">
-        <img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" style={{ width: "1em", marginInlineStart: "0.5em" }}  
+        <img src="/images/icons/orchid.avif" style={{ width: "1em", marginInlineStart: "0.5em" }}  
         alt="ORCID iD icon"/>
         https://orcid.org/0000-0002-2734-313X
         </SmartLink>
