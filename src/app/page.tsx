ea0ff0d4b883f90949cb94ae60c77ec3c13a359e
Dhruv <haldardhruv@gmail.com>
2025-03-23 13:28:30 +0530

    minify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,42 @@
-import React from "react";import Script from 'next/script';
-import { Heading, Flex, Text, Button, Avatar, RevealFx, Column } from "@/once-ui/components";import { Projects } from "@/components/work/Projects";
+import React from "react";
+import Script from 'next/script';
+import { Heading, Flex, Text, Button, Avatar, RevealFx, Column } from "@/once-ui/components";
+import { Projects } from "@/components/work/Projects";
 import { baseURL, routes } from "@/app/resources";
 import { home, about, person } from "@/app/resources/content";
 import { Posts } from "@/components/publications/Posts";
 
 export async function generateMetadata() {
-    const title = home.title;
-    const alt_title = 'Showcasing the Best Projects';
-    const description = home.description;
-    const ogImage = `https://${baseURL}/opengraph.jpg`;
-    const pageUrl = `https://${baseURL}`;
+  const title = home.title;
+  const alt_title = 'Showcasing the Best Projects';
+  const description = home.description;
+  const ogImage = `https://${baseURL}/opengraph.jpg`;
+  const pageUrl = `https://${baseURL}`;
   return {
-    title,description,
-      openGraph: {title,description,type: "website",url: pageUrl,siteName: `${person.firstName}'s Portfolio (With Projects & Publications)`,images: [{url: ogImage,width: 1200,height: 630,alt: alt_title,},],},twitter: {card: "summary_large_image",title,description,images: [ogImage],},};
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      url: pageUrl,
+      siteName: `${person.firstName}'s Portfolio (With Projects & Publications)`,
+      images: [{ url: ogImage, width: 1200, height: 630, alt: alt_title }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+    },
+  };
 }
 
 export default function Home() {
   return (
     <Column maxWidth="m" gap="xl" horizontal="center">
+      // Umami Analytics
       <Script defer src="https://cloud.umami.is/script.js" data-website-id="bdc0e551-96ce-4161-8e5b-3e9e89a304a2"></Script>
-      {/* Umami Analytics */}
       <script
         type="application/ld+json"
         suppressHydrationWarning
@@ -50,12 +67,12 @@ export default function Home() {
             </Heading>
           </RevealFx>
           <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
-            <Text wrap="balance" onBackground="neutral-medium" variant="heading-default-xl" >
+            <Text wrap="balance" onBackground="neutral-medium" variant="heading-default-xl">
               {home.subline}
             </Text>
           </RevealFx>
           <RevealFx translateY="12" delay={0.4} horizontal="start">
-            <Button id="about" data-border="rounded" href="/about" variant="secondary" size="m"arrowIcon>
+            <Button id="about" data-border="rounded" href="/about" variant="secondary" size="m" arrowIcon>
               <Flex gap="8" vertical="center">
                 {about.avatar.display && (
                   <Avatar
@@ -75,17 +92,4 @@ export default function Home() {
       </RevealFx>
       {routes["/blog"] && (
         <Flex fillWidth gap="24" mobileDirection="column">
-          <Flex flex={1} paddingLeft="l">
-            <Heading as="h1" variant="display-strong-xs" wrap="balance">
-              Latest from the blog
-            </Heading>
-          </Flex>
-          <Flex flex={3} paddingX="20">
-            <Posts range={[1, 2]} columns="2" />
-          </Flex>
-        </Flex>
-      )}
-      <Projects range={[2]} />
-    </Column>
-  );
-}
+          <Flex flex={1} paddingLeft="l"><Heading as="h1" variant="display-strong-xs" wrap="balance">Latest from the blog</Heading></Flex><Flex flex={3} paddingX="20"><Posts range={[1, 2]} columns="2" /></Flex></Flex>)}<Projects range={[2]} /></Column>);}
