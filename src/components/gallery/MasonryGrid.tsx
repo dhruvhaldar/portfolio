228251d6fb484677e84e41ca2daba45f77e4ea29
Dhruv <haldardhruv@gmail.com>
2025-03-29 03:45:51 +0530

    refactor: improve styling and replace dynamic photo fetching with static gallery content

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -1,32 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
 import Masonry from "react-masonry-css";
 import { SmartImage } from "@/once-ui/components";
 import styles from "./Gallery.module.scss";
-
-const fetchFlickrPhotos = async () => {
-  const apiKey = process.env.NEXT_PUBLIC_FLICKR_API_KEY;
-  const userId = process.env.NEXT_PUBLIC_FLICKR_USER_ID;
-  const response = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`);
-  const data = await response.json();
-  return data.photos.photo.map(photo => ({
-    src: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-    alt: photo.title,
-    orientation: 'horizontal', // You might need to determine this based on photo dimensions
-  }));
-};
+import { gallery } from "@/app/resources/content";
 
 export default function MasonryGrid() {
-  const [photos, setPhotos] = useState<{ src: string; alt: string; orientation: string }[]>([]);
-
-  useEffect(() => {
-    const loadPhotos = async () => {
-      const flickrPhotos = await fetchFlickrPhotos();
-      setPhotos(flickrPhotos);
-    };
-    loadPhotos();
-  }, []);
-
   const breakpointColumnsObj = {
     default: 4,
     1440: 3,
@@ -40,7 +19,7 @@ export default function MasonryGrid() {
       className={styles.masonryGrid}
       columnClassName={styles.masonryGridColumn}
     >
-      {photos.map((image, index) => (
+      {gallery.images.map((image, index) => (
         <SmartImage
           priority={index < 10}
           sizes="(max-width: 560px) 100vw, (max-width: 1024px) 50vw, (max-width: 1440px) 33vw, 25vw"
