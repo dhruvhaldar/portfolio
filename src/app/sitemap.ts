2fbb1f657c8e597167b26865bab6e63d1b76cf49
Dhruv <haldardhruv@gmail.com>
2025-03-28 19:55:31 +0530

    refactor: improve code formatting and readability in NotFound, sitemap, manifest, types, and Footer components

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,2 +1,27 @@
-import { getPosts } from "@/app/utils/utils";import { baseURL, routes as routesConfig } from "@/app/resources";
-export default async function sitemap() {const publications = getPosts(["src", "app", "publications", "posts"]).map((post) => ({url: `https://${baseURL}/publications/${post.slug}`,lastModified: post.metadata.publishedAt,}));const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({url: `https://${baseURL}/work/${post.slug}`,lastModified: post.metadata.publishedAt,}));const activeRoutes = Object.keys(routesConfig).filter((route) => routesConfig[route]);const routes = activeRoutes.map((route) => ({url: `https://${baseURL}${route !== "/" ? route : ""}`,lastModified: new Date().toISOString().split("T")[0],}));return [...routes, ...publications, ...works];}
+import { getPosts } from "@/app/utils/utils";
+import { baseURL, routes as routesConfig } from "@/app/resources";
+
+export default async function sitemap() {
+  const publications = getPosts(["src", "app", "publications", "posts"]).map(
+    (post) => ({
+      url: `https://${baseURL}/publications/${post.slug}`,
+      lastModified: post.metadata.publishedAt,
+    })
+  );
+
+  const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
+    url: `https://${baseURL}/work/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
+
+  const activeRoutes = Object.keys(routesConfig).filter(
+    (route) => routesConfig[route]
+  );
+
+  const routes = activeRoutes.map((route) => ({
+    url: `https://${baseURL}${route !== "/" ? route : ""}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  return [...routes, ...publications, ...works];
+}
