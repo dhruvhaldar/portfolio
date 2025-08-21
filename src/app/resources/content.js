adcd98846bbe0301b8e2b05b18e51d32ad666b21
Dhruv Haldar <haldardhruv@gmail.com>
2025-08-21 15:06:12 +0530

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -60,7 +60,9 @@ const home = {
   subline: (
     <>
       👋 Hi! I&apos;m Dhruv Haldar, an Aerospace/CFD engineer at{" "}
-      <InlineCode>TRIDIAGONAL SOLUTIONS</InlineCode> 🏭 where I specialize in simulations and application testing. I have a proven track record of successfully tackling challenging engineering issues in the fields of aerospace 🛩️, chemical 🧪, automotive 🏎️, and propulsion 🚀 systems.
+      <InlineCode>
+      <a href="https://www.tridiagonal.com/">Tridiagonal Software Inc.</a>
+    </InlineCode> 🏭 where I specialize in simulations and application testing. I have a proven track record of successfully tackling challenging engineering issues in the fields of aerospace 🛩️, chemical 🧪, automotive 🏎️, and propulsion 🚀 systems.
     </>
   ),
 };
